4ec7c9a34da2536b91a0ddae3a48f45532aff382
wangsupershuang <wangsupershuang@126.com>
2018-01-14 22:39:21 +0800

    Add files via upload

---
+++ src/main/java/com/hadoop/hadoopdemo/HadoopdemoApplication.java
@@ -0,0 +1,12 @@
+package com.hadoop.hadoopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopdemoApplication.class, args);
+	}
+}
